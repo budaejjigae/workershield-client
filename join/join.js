f7bc37f31a5ce9abd0bcf15f5d050e2728a80c74
hyeg <hyeg0121@naver.com>
2023-08-06 05:56:25 +0900

    feat: 회원가입 기능 구현

--- join/join.js
+++ join/join.js
@@ -1,5 +1,41 @@
+const userNameInput = document.getElementById('name-input');
+const userIDInput = document.getElementById('id-input');
+const userPWInput = document.getElementById('pw-input');
+const userPwCheckInput = document.getElementById('pw-check-input');
+const inviteCodeInput = document.getElementById('invite-input');
+
 const prevButton = document.getElementById('prev-button');
+const joinForm = document.getElementById('join-form');
+const errorMessages = document.getElementsByClassName('error-msg');
 
 prevButton.onclick = () => {
     window.open('../login/login.html', '_top');
 };
+
+joinForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const userName = userNameInput.value;
+    const userID = userIDInput.value;
+    const userPW = userPWInput.value;
+    const userPwCheck = userPwCheckInput.value;
+    const inviteCode = inviteCodeInput.value;s
+
+    const data = {userID, userName, userPW, inviteCode};
+
+    axios.post('http://192.168.10.192:8088/auth/user', data)
+    .then(response => {
+        if(response.data.statusMsg){
+            console.log('성공');
+            window.open('../login', '_top');
+        }else{
+            console.log('실패');
+        }
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+
+
+});
+
