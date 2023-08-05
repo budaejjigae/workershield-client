135008ca2949c8aba2397524c84dbbefbc960507
hyeg <hyeg0121@naver.com>
2023-08-06 03:39:53 +0900

    feat: article 세부 페이지 구현 및 경로 변경

---
+++ info/articles/article.js
@@ -0,0 +1,17 @@
+const urlParam = new URL(location.href).searchParams.get('id');
+const id = Number(urlParam);
+
+const mainTitle = document.getElementsByClassName('main-title')[0];
+const thumbnail = document.getElementsByClassName('image-title')[0];
+const articleTitle = document.getElementById('article-title');
+const articleContent = document.getElementById('article-content');
+
+for(let el of data.articles){
+    console.log(el);
+    if(el.id === id){
+        thumbnail.style.backgroundImage = `url(${el.thumbnail})`; 
+        mainTitle.innerHTML= el.title;
+        articleTitle.innerHTML = el.title;
+        articleContent.innerHTML = el.content;
+    }
+}
